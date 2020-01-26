e1e8b03a0145a4d03ab8a9ac7a4977ced9066c03
Ashesh Bharadwaj <ppashesh@gmail.com>
2020-01-26 18:47:46 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/layoutwithconstraintlayout/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.layoutwithconstraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
